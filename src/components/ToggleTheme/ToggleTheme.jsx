fe3c922f183aa835b2d7306748250e8e68eeab4d
Cléber Severo <clebersevero1998@gmail.com>
2023-01-08 22:25:50 -0300

    Dark mode

---
+++ src/components/ToggleTheme/ToggleTheme.jsx
@@ -0,0 +1,17 @@
+import { MoonIcon, SunIcon } from '@heroicons/react/24/solid'
+import React from 'react'
+
+const ToggleTheme = () => {
+
+    const toggle = () => {
+        document.documentElement.classList.toggle('dark')
+    }
+  return (
+    <div className='hidden sm:block'>
+        <MoonIcon className='h-8 text-gray-100 block cursor-pointer dark:hidden' onClick={toggle}/>
+        <SunIcon className='h-8 text-gray-100 hidden cursor-pointer dark:block' onClick={toggle}/>
+        </div>
+  )
+}
+
+export default ToggleTheme

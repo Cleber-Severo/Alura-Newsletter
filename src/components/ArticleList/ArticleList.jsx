fb35ce5a590df44be21c61d8e3e647b727211b30
Cléber Severo <clebersevero1998@gmail.com>
2023-01-16 23:08:04 -0300

    cards created based on data info

--- src/components/ArticleList/ArticleList.jsx
+++ src/components/ArticleList/ArticleList.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import data from '../../../articles.json'
+import Article from '../article/Article'
 
 const ArticleList = () => {
   return (
-    <div className='mt-5 grid gap-5 m-auto max-w-2xl sm:mt-0 sm:grid-cols-2 lg:grid-cols-3'>
-       {data.map( (props,index) => <p key={index}>{props.title}</p> )}
+    <div className='mt-5 grid gap-5 m-auto max-w-2xl sm:mt-0 sm:grid-cols-2 lg:grid-cols-3 lg:max-w-[1000px]'>
+       {
+        data.map( (props,index) => <Article key={index} {...props}/> )
+      }
     </div>
   )
 }

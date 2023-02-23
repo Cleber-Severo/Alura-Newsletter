60df6e07a9281ba0a110ad35353f7ea65c0c46aa
Cléber Severo <clebersevero1998@gmail.com>
2023-02-23 20:02:55 -0300

    Hover effects for the submit button

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -14,7 +14,7 @@ const Form = ({ onSubmit}) => {
     <form onSubmit={safeSubmit} className='h-full flex flex-col items-center justify-center gap-10 -mt-20'>
         <Input type="text" required placeholder='Insira seu nome'/>
         <Input type="email"  required placeholder='Insira seu email'/>
-        <button type='submit' className='bg-alura-100 dark:bg-dark-200 rounded-full w-full max-w-sm text-gray-200 dark:text-gray-200 uppercase px-5 py-1'>Seguir</button>
+        <button type='submit' className='bg-alura-100 dark:bg-dark-200 rounded-full w-full max-w-sm text-gray-200 dark:text-gray-200 uppercase px-5 py-1 outline-none hover:animate-pulse hover:shadow-md hover:shadow-gray-500 dark:hover:shadow-black'>Seguir</button>
     </form>
   )
 }

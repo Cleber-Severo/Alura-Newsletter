ebf468301340db767861b529be08be3f44f664cd
Cléber Severo <clebersevero1998@gmail.com>
2023-02-26 12:42:27 -0300

    layer components and apply to reutilize syling on multiple tags

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -12,8 +12,8 @@ const Form = ({ onSubmit}) => {
     }
     return (
     <form onSubmit={safeSubmit} className='h-full flex flex-col items-center justify-center gap-10 -mt-20'>
-        <Input type="text" required placeholder='Insira seu nome'/>
-        <Input type="email"  required placeholder='Insira seu email'/>
+        <input type="text" className='alura-input' required placeholder='Insira seu nome'/>
+        <input type="email" className='alura-input' required placeholder='Insira seu email'/>
         <button type='submit' className='bg-alura-100 dark:bg-dark-200 rounded-full w-full max-w-sm text-gray-200 dark:text-gray-200 uppercase px-5 py-1 outline-none hover:animate-pulse hover:shadow-md hover:shadow-gray-500 dark:hover:shadow-black'>Seguir</button>
     </form>
   )

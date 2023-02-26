5178179620d93c44385c3ae1671a9db88f558c6b
Cléber Severo <clebersevero1998@gmail.com>
2023-02-26 13:42:25 -0300

    More layer components styling and Layer Base

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Input from '../Input/Input';
 
 const Form = ({ onSubmit}) => {
   
@@ -14,7 +13,7 @@ const Form = ({ onSubmit}) => {
     <form onSubmit={safeSubmit} className='h-full flex flex-col items-center justify-center gap-10 -mt-20'>
         <input type="text" className='alura-input' required placeholder='Insira seu nome'/>
         <input type="email" className='alura-input' required placeholder='Insira seu email'/>
-        <button type='submit' className='bg-alura-100 dark:bg-dark-200 rounded-full w-full max-w-sm text-gray-200 dark:text-gray-200 uppercase px-5 py-1 outline-none hover:animate-pulse hover:shadow-md hover:shadow-gray-500 dark:hover:shadow-black'>Seguir</button>
+        <button type='submit' className='alura-button'>Seguir</button>
     </form>
   )
 }

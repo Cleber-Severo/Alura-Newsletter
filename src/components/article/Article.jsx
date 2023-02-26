70f855c16e24c0a7198d007c718a8ca8febda794
Cléber Severo <clebersevero1998@gmail.com>
2023-02-26 13:50:54 -0300

    working with gradients

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -5,7 +5,7 @@ const Article = ({text, title, tags, image, alt}) => {
     <div className='alura-card'>
         <h3 className='text-xl text-alura-200 dark:text-gray-200 font-bold'>{title}</h3>
         <div className='w-full hidden sm:flex sm:flex-row sm:justify-end sm:gap-2 pr-5'>
-          {tags.map(tag => <span key={tag} className='bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full text-gray-200 dark:text-gray-200 text-xs uppercase font-bold hover:bg-alura-200 hover:scale-110' >{tag}</span>)}
+          {tags.map(tag => <span key={tag} className='alura-tag' >{tag}</span>)}
         </div>
         
         <div className='grid  gap-1'>
